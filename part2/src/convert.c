235beadfae5991c575846aff32f0f6465a9a5d7a
Kevin <kl3451@columbia.edu>
2023-02-06 17:34:24 -0500

    part2 completed

--- part2/src/convert.c
+++ part2/src/convert.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    return EXIT_SUCCESS;
+void print_binary(unsigned int n) {
+	int i;
+	for(i = 31;i >= 0;i--) {
+		putc(((n >> i) & 1) + '0',stdout);
+		if(i % 4 == 0) {
+			putc(' ',stdout);
+		}
+	}
+	putc('\n',stdout);
 }
+
+int main(void) {
+    int x;
+    printf("Enter an integer : ");
+    scanf("%d", &x);
+    printf("\nsigned decimal   : %d\n", x);
+    printf("unsigned decimal : %u\n", (unsigned int) x);
+    printf("hexadecimal      : %x\n", (unsigned int) x);
+    printf("binary           : ");
+    print_binary((unsigned int) x);
+    putc('\n', stdout);
+    return 0;
+}
+
