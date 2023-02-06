2bd289be724819dec04aad10f070bbac69ca5741
Kevin <kl3451@columbia.edu>
2023-02-06 16:32:25 -0500

    part1 completed

--- part1/src/gcd.c
+++ part1/src/gcd.c
@@ -4,5 +4,22 @@
 #include "recursive.h"
 
 int main(int argc, char **argv) {
-    return EXIT_SUCCESS;
+	if( argc != 3) {
+	   fprintf(stderr, "Usage: %s <integer m> <integer n>\n", argv[0]);
+	return EXIT_FAILURE;
+	}
+	int m = atoi(argv[1]);
+	int n = atoi(argv[2]);
+
+	if(m == 0 && n == 0) {
+		printf("gcd(0,0) = undefined\n");
+		return EXIT_SUCCESS;
+	}
+
+	int iterative = gcd_iterative(m,n);
+	int recursive = gcd_recursive(m,n);
+	
+	printf("Iterative: gcd(%d, %d) = %d\n", m, n, iterative);
+	printf("Recursive: gcd(%d, %d) = %d\n", m, n, recursive);
+    	return EXIT_SUCCESS;
 }
--- part1/src/iterative.h
+++ part1/src/iterative.h
@@ -1,1 +1,5 @@
+#ifndef ITERATIVE_H
+#define ITERATIVE_H
 int gcd_iterative(int m, int n);
+
+#endif
