598e37cd0fa713e05151532a8177f89b6ec4c6be
Yucheng Lu <yl5163@columbia.edu>
2023-02-07 01:39:39 -0500

    test 5

---
+++ part2/src/convert.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+        int x;
+        printf("Enter an integer : ");
+        scanf(" %d", &x);
+        fflush(stdin);
+ 	printf("\nsigned decimal   : %d\n", x);
+        printf("unsigned decimal : %u\n", (unsigned int)x);
+        printf("hexadecimal      : %x\n", (unsigned int)x);
+        printf("binary           : ");
+        unsigned int mask = 1u << 31;
+        int count = 0;
+        for (int i = 31; i >= 0; i--) {
+                count++;
+                if (((unsigned int)x & mask) == 0)
+                        putc('0', stdout);
+                else
+                        putc('1', stdout);
+                if ((count & 0x3) == 0)
+                        putc(' ', stdout);
+                mask >>= 1;
+    }
+    putc('\n', stdout);
+    return 0;
+}
